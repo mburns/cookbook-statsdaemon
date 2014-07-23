aeb7986348755f8cfc8962c00406a67a379d9734
Olivier Dolbeau <contact@odolbeau.fr>
2014-07-23 11:50:19 +0200

    Correct metadata

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs statsdaemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-recipe 'golang', 'Installs statsdaemon.'
+recipe 'statsdaemon', 'Installs statsdaemon.'
 
 supports 'debian'
 supports 'ubuntu'
