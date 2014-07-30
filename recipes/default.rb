7c30301a5c391d074539379afddd4d6b4cd7abad
Olivier Dolbeau <contact@odolbeau.fr>
2014-07-30 12:28:23 +0200

    Any change in the init.d should restart the service

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,18 +24,19 @@ golang_package 'github.com/bitly/statsdaemon' do
   action :install
 end
 
-template '/etc/init.d/statsdaemon' do
-  source 'statsdaemon_init.erb'
-  mode 0755
-  owner 'root'
-  group 'root'
-end
-
 file '/var/log/statsdaemon.log' do
   owner node['go']['owner']
   group node['go']['group']
   mode '0644'
   action :create_if_missing
+end
+
+template '/etc/init.d/statsdaemon' do
+  source 'statsdaemon_init.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  notifies :reload, 'service[statsdaemon]', :delayed
 end
 
 service 'statsdaemon' do
