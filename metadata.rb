68330d6a60ffc3f0a4912ec2a036f43d38dee9b8
Olivier Dolbeau <contact@odolbeau.fr>
2014-08-26 14:02:17 +0200

    Rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             'statsdaemon'
+name             'statsdaemon-bitly'
 maintainer       'Olivier Dolbeau'
 maintainer_email 'odolbeau@gmail.com'
 license          'MIT'
-description      'Installs statsdaemon'
+description      'Installs statsdaemon (bitly)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
-recipe 'statsdaemon', 'Installs statsdaemon.'
+recipe 'default', 'Installs statsdaemon.'
 
 supports 'debian'
 supports 'ubuntu'
