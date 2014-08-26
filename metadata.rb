ee3336e4a21f30547c23a1ab73b325062b83207f
Olivier Dolbeau <contact@odolbeau.fr>
2014-08-26 14:43:33 +0200

    Bump to version 1.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'odolbeau@gmail.com'
 license          'MIT'
 description      'Installs statsdaemon (bitly)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 recipe 'default', 'Installs statsdaemon.'
 
